2992e4c9b3a1b19f709b1a273f4ace3b67b136ad
josesilvestrefernando-sketch <josesilvestrefernando@gmail.com>
2025-09-22 15:31:05 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,20 @@
+package com.processingaide;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public void settings() {
+        
+        fullScreen();
+    }
+
+    public void setup() {
+		background(255); // Fill the background with white
+    }
+
+    public void draw() {
+		fill(0); // Fill inside with black
+		rect(200, 400, 200, 200); // Draw a rectangle at position at (200, 400) 200x200 size
+    }   
+}
 
