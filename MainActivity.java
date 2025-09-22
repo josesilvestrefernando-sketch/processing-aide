327aca8b2c5340feaadf1b795c42b15163d172c1
josesilvestrefernando-sketch <josesilvestrefernando@gmail.com>
2025-09-22 15:32:52 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,1 +1,20 @@
+package com.processingaide;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment fragment = new Main();
+        fragmentManager.beginTransaction()
+			.replace(R.id.container, fragment)
+			.commit();
+    }
+}
 
